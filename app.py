bda702897f5a625ce98d60825454d00cf682c4e0
Ollix <thiago_as_monteiro@hotmail.com>
2025-03-18 19:27:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+# Load ingredient data
+with open("subway_full_nutrition.json", "r", encoding="utf-8") as file:
+    ingredients = json.load(file)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get_ingredients")
+def get_ingredients():
+    return jsonify(ingredients)
+
+if __name__ == "__main__":
+    app.run(debug=True)
